1ab13a6091d310c60f1df59ea02d7ef23a7e6670
pal711 <contactsouvik7@gmail.com>
2025-01-23 13:12:49 +0530

    removed simple trainer from other files

--- project/package/trainers/ListedTrainers.py
+++ project/package/trainers/ListedTrainers.py
@@ -1,8 +1,6 @@
-from package.trainers import SimpleTrainer as ST
 from package.trainers import UltimateTrainer as UT
 
 
 TRAINERS = {
-    # "SimpleTrainer": ST.SimpleTrainer,
     "UltimateTrainer": UT.UltimateTrainer
 }
